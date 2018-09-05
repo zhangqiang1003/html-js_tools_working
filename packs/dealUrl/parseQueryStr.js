1fde8bfb2e9b0119a86e282508340719d2cdb564
zhangqiang <zhangqiang@qeeyou.cn>
2018-09-05 09:22:07 +0800

    添加处理url查询字符串的新方法parseQueryStr

---
+++ packs/dealUrl/parseQueryStr.js
@@ -0,0 +1,21 @@
+/**
+ * 处理查询字符串
+ * tips:1.使用encodeURIComponent()函数转码url
+ */
+
+function parseQueryStr () {
+  var query = decodeURIComponent(window.location.search);
+  var obj = {};
+  var exp = /^\?/.test(query);
+  var arr = [];
+  if (exp) {
+      query.slice(1).split("&").forEach(function(val) {
+          arr = val.split("=");
+          obj[arr[0]] = arr[1];
+      })
+      return obj;
+  }
+  return
+}
+
+export default parseQueryStr
