e460f44c80cbc17964f3928e9deb71737f8f569c
zhangqiang <zhangqiang@qeeyou.cn>
2019-01-14 11:38:15 +0800

    [处理字符串的大小写]

--- packs/string/changeStrCase.js
+++ packs/string/changeStrCase.js
@@ -5,46 +5,55 @@
  * @return {String} 新的字符串
  */
 
-export function changeStrCase(str, type) {
-    upperCase(_str) {
-        //首字母大写
-        return _str.substring(0,1).toUpperCase() + _str.substring(1);
+export function ChangeStrCase(str, type) {
+  this.str = str;
+  this.type = type;
+  this.init();
+}
+// 初始化
+ChangeStrCase.prototype.init = function() {
+  switch (this.type) {
+    case 1:
+      return this.upperCase(this.str);
+      break;
+    case 2:
+      return this.lowerCase(this.str);
+      break;
+    case 3:
+      return this.toggleCase(this.str);
+      break;
+    case 4:
+      return this.str.toUpperCase();
+      break;
+    case 5:
+      return this.str.toLowerCase();
+      break;
+    default:
+      return this.str;
+  }
+}
+
+// 首字母大写
+ChangeStrCase.prototype.upperCase = function() {
+  return this.str.substring(0,1).toUpperCase() + this.str.substring(1);
+}
+
+// 首字母小写
+ChangeStrCase.prototype.lowerCase = function() {
+  return this.str.substring(0,1).toLowerCase() + this.str.substring(1);
+}
+
+// 大小写切换
+ChangeStrCase.prototype.toggleCase = function() {
+  let itemText = '';
+  this.str.split("").forEach(val => {
+    if (/^[a-z]$/.test(val)) {
+      itemText += val.toUpperCase();
+    } else if (/^[A-Z]$/.test(val)) {
+      itemText += val.toLowerCase();
+    } else {
+      itemText += val;
     }
-    lowerCase(_str) {
-        //首字母小写
-        return _str.substring(0,1).toLowerCase() + _str.substring(1);
-    }
-    toggleCase(_str) {
-        //大小写切换
-        let itemText = "";
-        _str.split("").forEach(val => {
-            if (/^[a-z]$/.test(val)) {
-                itemText += val.toUpperCase();
-            } else if (/^[A-Z]$/.TEST(val)) {
-                itemText += val.toLowerCase();
-            } else {
-                itemText += val;
-            }
-        });
-        return itemText;
-    }
-    switch (type) {
-      case 1:
-        return upperCase(str);
-        break;
-      case 2:
-        return lowerCase(str);
-        break;
-      case 3:
-        return toggleCase(str);
-        break;
-      case 4:
-        return str.toUpperCase();
-        break;
-      case 5:
-        return str.toLowerCase();
-        break;
-      default:
-        return str;
-    }
+  });
+  return itemText;
 }
